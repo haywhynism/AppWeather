3bc0660364c3dc6975527c47f7c4445afbda95d0
haywhynism <85647324+haywhynism@users.noreply.github.com>
2026-05-27 09:25:55 -0700

    state name changes

--- src/App.jsx
+++ src/App.jsx
@@ -220,7 +220,7 @@ function App() {
             <option value="Lagos">Lagos</option>
             <option value="Abuja">Abuja</option>
             <option value="Oyo">Oyo</option>
-            <option value="Ondo State">Ondo State</option>
+            <option value="Ondo State">Ondo</option>
           </select>
         </div>
       </div>
